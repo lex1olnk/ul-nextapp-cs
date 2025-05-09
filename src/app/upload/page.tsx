7d490b672dd9aa9d9b5d8ff5a6c2ab5deaaa4bec
lex1olnk <64180205+lex1olnk@users.noreply.github.com>
2025-05-09 16:47:44 +0300

    updating ultournaments in update page

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,6 +3,8 @@
 import MatchImporter from "@/components/tournaments/MatchImporter";
 import { getTournaments } from "./api";
 
+export const revalidate = 3600;
+
 export default async function UploadMatchesPage() {
   const tournaments = await getTournaments();
 
